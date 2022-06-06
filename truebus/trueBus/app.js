be2bf5a4094175acf3af59858a679d835c505715
Abhinav Tv <89732408+22abhinavtv@users.noreply.github.com>
2022-06-06 10:04:03 +0530

    Update app.js

--- truebus/trueBus/app.js
+++ truebus/trueBus/app.js
@@ -3,26 +3,29 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var loginRouter = require('./routes/login');
-
+var hbs = require('express-handlebars')
 var app = express();
+
+var userRouter = require('./routes/user');
+var adminRouter = require('./routes/admin');
+
+//app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.use('/', userRouter);
+app.use('/admin', adminRouter);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
+app.set('view engine', 'hbs');
+app.engine('hbs',hbs.engine({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layout/',partialsDir:__dirname+'/views/partials/'}))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname + 'public')));
+app.use('/javascript',express.static('./public/javascript'))
+app.use('/images',express.static('./public/images'))
+app.use('/styles',express.static('./public/styles'))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/login',loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,3 +44,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
